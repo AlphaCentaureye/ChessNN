6dbda1f7377148a3abf9ccc7e52d91309e9e047f
William Coffer <wzcoffer@gmail.com>
2023-11-20 15:23:40 -0800

    files added

---
+++ src/functions.py
@@ -0,0 +1,14 @@
+import chess
+
+def play(move_string, board):
+  move = chess.Move.from_uci(move_string)
+  if not(move in board.legal_moves):
+    return False
+  else:
+    try:
+      board.push(move)
+      display(board)
+      return True
+    except Exception as e:
+      print(e)
+      return False
