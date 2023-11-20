c4f8220fdee96fc09268f9aa178a5a9ea13c3074
William Coffer <wzcoffer@gmail.com>
2023-11-20 15:31:58 -0800

    test

---
+++ src/nn_functions.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+def define_model():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.InputLayer(input_shape=(8,8, 13), name="input_layer"))
+    model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=2, activation='relu', name="block1_conv1"))
+    return model
