0cc75fb189c934a33525ada2b4cac2ec6fc167ed
William Coffer <wzcoffer@gmail.com>
2023-11-20 16:21:53 -0800

    neural network architecture added

--- src/nn_functions.py
+++ src/nn_functions.py
@@ -1,7 +1,26 @@
 import tensorflow as tf
 
 def define_model():
-    #model = tf.keras.Sequential()
+    model = tf.keras.Sequential()
     model.add(tf.keras.layers.InputLayer(input_shape=(8,8, 13), name="input_layer"))
-    model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=2, activation='relu', name="block1_conv1"))
+    model.add(tf.keras.layers.Conv2D(filters=16, kernel_size=1, activation='relu', name="block1_conv1"))
+    model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=1, activation='relu', name="block1_conv2"))
+    model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=1, activation='relu', name="block1_conv3"))
+    model.add(tf.keras.layers.Resizing(height=128, width=128, interpolation='bilinear', crop_to_aspect_ratio=False))
+    model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=1, activation='relu', name="block2_conv1"))
+    model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=1, activation='relu', name="block2_conv2"))
+    model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=1, activation='relu', name="block2_conv3"))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Conv2D(filters=256, kernel_size=1, activation='relu', name="block3_conv1"))
+    model.add(tf.keras.layers.Conv2D(filters=512, kernel_size=1, activation='relu', name="block3_conv2"))
+    model.add(tf.keras.layers.Conv2D(filters=512, kernel_size=1, activation='relu', name="block3_conv3"))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(5, 5)))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(units=1024, activation='relu'))
+    model.add(tf.keras.layers.Dense(units=1024, activation='relu',))
+    model.add(tf.keras.layers.Dense(132, activation='softmax', name="output_layer"))
     return model
+
+def test_gpu():
+    print(tf.test.gpu_device_name())
+
