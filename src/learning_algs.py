ce0c9b2b64b519eca3c40f26301df3a9b2831f2f
William Coffer <wzcoffer@gmail.com>
2023-12-03 21:00:49 -0800

    added capability to change exploration rate ratio

--- src/learning_algs.py
+++ src/learning_algs.py
@@ -15,23 +15,23 @@ class Q_learn(object):
         self.reward_trace = []
         self.samp_probabilities = []
 
-    def learn(self, iterations=100, updateThreshold=10):
+    def learn(self, iterations=100, updateThreshold=10, explorationRateRatio=250):
         for x in range(iterations):
             if x % updateThreshold == 0:
                 print("iteration: ", x)
                 self.agent.freeze_model()
             greedy = True if x == iterations - 1 else False
             self.env.reset()
-            self.play(x, greedy=greedy)
+            self.play(x, greedy=greedy, explorationRateRatio=explorationRateRatio)
 
         pgn = Game.from_board(self.env.board)
         return pgn
     
-    def play(self, explorationRate, greedy=False, maxMoves=300):
+    def play(self, explorationRate, greedy=False, maxMoves=300, explorationRateRatio=250):
         # max moves is defaulted to 300 as that should never interfere normally, but should prevent it from going on too long in initial training
         keep_going = True
         turnNumber = 0
-        epsilonGreedy = max(0.05, 1 / (1 + (explorationRate / 250))) if not(greedy) else 0.0
+        epsilonGreedy = max(0.05, 1 / (1 + (explorationRate / explorationRateRatio))) if not(greedy) else 0.0
         while keep_going:
             state = Agent.one_hot_encode(self.env.board, chess.WHITE) # white for now
             explore = np.random.uniform(0,1) < epsilonGreedy
