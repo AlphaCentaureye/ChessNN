e0986a105d24a052ecb4077bbeca96243a84607f
William Coffer <wzcoffer@gmail.com>
2023-12-06 15:10:45 -0800

    training for pc

--- src/main.py
+++ src/main.py
@@ -1,13 +1,26 @@
-import tensorflow as tf
 import os
+import tensorflow as tf
 import chess
 from environment import Board
 from agent import Agent
+from learning_algs import Q_learn
+import numpy as np
 
-agent = Agent("w")
-board = Board()
+
+
+# train network
+
+environment = Board()
+agent = Agent(verbose=1)
 agent.init_network()
-#agent.model.summary()
-agent.one_hot_encode(board.board)
+#print(agent.model.summary())
 
-#agent.saveNN()
+learn = Q_learn(agent, environment)
+learn.learn(iterations=30, updateThreshold=1, explorationRateRatio=5, display=True)
+
+try:
+    agent.saveNN() # optional path param, but defaults to path /content/savedNNs/
+except Exception as e:
+    print(e)
+
+
