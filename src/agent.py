5f898e499d74eda864ec8619237747d740884263
William Coffer <wzcoffer@gmail.com>
2023-12-07 14:14:41 -0800

    frozen model also uses categorical crossentropy

--- src/agent.py
+++ src/agent.py
@@ -83,7 +83,7 @@ class Agent(object):
   def freeze_model(self):
     self.frozen_model = tf.keras.models.clone_model(self.model)
     opt = tf.keras.optimizers.SGD(learning_rate=self.lr, momentum=0.0, weight_decay=0.0)
-    self.frozen_model.compile(optimizer=opt, loss='mse', metrics=['mae'])
+    self.frozen_model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
     self.frozen_model.set_weights(self.model.get_weights())
 
   def find_move(self, state):
