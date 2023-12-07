452c32b453b2b358f1e4ee50c3596cb6c863723a
William Coffer <wzcoffer@gmail.com>
2023-12-06 18:12:58 -0800

    saveNN change

--- src/agent.py
+++ src/agent.py
@@ -133,7 +133,7 @@ class Agent(object):
     try:
       path = os.path.join(savePath, '/nn_model')
       if not(os.path.exists(path)):
-        os.mkdir(savePath)
+        os.mkdir(path)
       self.model.save(path)
       with zipfile.ZipFile(os.path.join(savePath, "/chessNN_model.zip"), 'w') as zip_ref:
         zip_ref.write(os.path.join(savePath, "/nn_model"))
